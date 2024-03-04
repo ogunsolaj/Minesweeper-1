f58955c6a902df85440acafb1b8d6a3359282120
ogunsolaj <johnogunsola112@gmail.com>
2024-03-04 18:54:05 +0000

    First test that grid is not empty

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,31 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+import java.util.function.BooleanSupplier;
 
 
 class MainTest {
     @Test
-    void Createsa10by10Arraylist() {
+    public void Createsa10by10Arraylist() {
+   Main main = new Main();
+        int rows = 10;
+        int cols = 10;
+        Cell[][] grid = new Cell[rows][cols];
+        int value = 1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = new Cell(i, j);
+                value++;
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int i1=0; i1<10; i1++) {
+            for (int j1=0; j1<10; j1++) {
+                Assertions.assertFalse(false,grid[i1][j1].toString());
 
+            }
+        }
     }
 
 }
